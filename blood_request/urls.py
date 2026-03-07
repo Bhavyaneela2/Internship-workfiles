bee73973facfd01121ce43c95c22986cd5d48cb0
Prakhar Shukla <prakharshukla297@gmail.com>
2026-03-07 21:51:28 +0530

    Add Contact Us and Our Team pages with layout, styling, and team member sections

--- blood_request/urls.py
+++ blood_request/urls.py
@@ -38,4 +38,9 @@ urlpatterns = [
 
    path('our-mission-values/', views.our_mission_values, name='our_mission_values'),
 
+     path('', views.home_view, name='home'),
+
+    path('contact-us/', views.contact_us, name='contact_us'),
+
+    path('our-team/', views.our_team, name='our_team'),
 ]
